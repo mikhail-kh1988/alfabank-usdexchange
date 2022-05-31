0c3386e8b5f97b80851c4b31cb85eb843a8d4405
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2022-05-31 12:16:12 +0300

    Initial commit

---
+++ src/main/java/ru/alfabank/AlfabankUsdExchangeApplication.java
@@ -0,0 +1,13 @@
+package ru.alfabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfabankUsdExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfabankUsdExchangeApplication.class, args);
+	}
+
+}
